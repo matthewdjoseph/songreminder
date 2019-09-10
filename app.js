89c7255dcfd5b65e6f5f84b76dfb6c6b911d76e0
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-09-09 21:28:00 -0400

    js outlined...the work continues

---
+++ app.js
@@ -0,0 +1,50 @@
+var songList = {
+    songs: [],
+    addSong: function(){
+
+    },
+    changeSong: function(){
+
+    },
+    deleteSong: function(){
+
+    },
+    toggleListened: function(){
+
+    },
+    toggleAll: function(){
+
+    }
+};
+
+var handlers = {
+    addSong: function(){
+
+    },
+    changeSong: function(){
+
+    },
+    deleteSong: function(){
+
+    },
+    toggleListened: function(){
+
+    },
+    toggleAll: function(){
+        
+    }
+};
+
+var view = {
+    displaySongs: function(){
+
+    },
+    createDeleteButton: function(){
+
+    },
+    setupEventListeners: function(){
+
+    }
+};
+
+view.setUpEventListeners();
